482828cd27d8f922cbf679a9d0711cf50a83c02a
Ryan LaBelle <40902420+rml14b@users.noreply.github.com>
2018-11-12 20:33:52 -0500

    Implemented Signin

--- application/app/src/main/java/edu/fsu/cs/mobile/nudge/MainActivity.java
+++ application/app/src/main/java/edu/fsu/cs/mobile/nudge/MainActivity.java
@@ -4,19 +4,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    EditText mEmail;
+    EditText mPassword;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEmail = (EditText) findViewById(R.id.email_editText);
+        mPassword = (EditText) findViewById(R.id.password_editText);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
     // method to handle SignIn Button click
     public void signInButton(View view){
+        // query database - check if user exists
+        // if not prompt error
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        Boolean submitFlag = true;
+
+        if(email.matches("")){
+            mEmail.setError("Field cannot be blank");
+            submitFlag = false;
+        }
+        if(password.matches("")){
+            mPassword.setError("Field cannot be blank");
+            submitFlag = false;
+        }
+
+        if(password.length() < 6){
+            mPassword.setError("Field must be longer than 6 characters");
+            submitFlag = false;
+        }
+
+        if(submitFlag){
+            firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener
+                    (new OnCompleteListener<AuthResult>() {
+                         public void onComplete(@NonNull Task<AuthResult> task) {
+                             if(task.isSuccessful()) {
+                                 Intent intent = new Intent(MainActivity.this, Home.class);
+                                 startActivity(intent);
+                             }
+                             else {
+                                 Toast.makeText(MainActivity.this, "Sign In Failed!", Toast.LENGTH_LONG).show();
+                             }
+                         }
+                    });
+        }
+
+        //Intent intent = new Intent(this, Home.class);
+        //startActivity(intent);
+    }
+
+    public void signUpButton(View view){
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
