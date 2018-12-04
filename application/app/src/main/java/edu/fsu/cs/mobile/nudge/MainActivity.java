2ea5995d6461b46411c49d95fbf6f6df4b65810e
Felippe Lossano <fcl15@my.fsu.edu>
2018-12-03 19:51:42 -0500

    sign in page edits

--- application/app/src/main/java/edu/fsu/cs/mobile/nudge/MainActivity.java
+++ application/app/src/main/java/edu/fsu/cs/mobile/nudge/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.fsu.cs.mobile.nudge;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +16,13 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import android.util.Log;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
     EditText mEmail;
     EditText mPassword;
     private FirebaseAuth firebaseAuth;
+    private VideoView aVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +32,20 @@ public class MainActivity extends AppCompatActivity {
         mEmail = (EditText) findViewById(R.id.email_editText);
         mPassword = (EditText) findViewById(R.id.password_editText);
         firebaseAuth = FirebaseAuth.getInstance();
+
+        aVideoView = (VideoView) findViewById(R.id.nVideoView);
+
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+ R.raw.nudgevideo);
+
+        aVideoView.setVideoURI(uri);
+        aVideoView.start();
+
+        aVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
     }
 
     // method to handle SignIn Button click
@@ -52,28 +70,28 @@ public class MainActivity extends AppCompatActivity {
             Log.i("submit", "submit Flag == true");
             firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener
                     (new OnCompleteListener<AuthResult>() {
-                         public void onComplete(@NonNull Task<AuthResult> task) {
-                             if(task.isSuccessful()) {
-                                 Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                                 startActivity(intent);
-                             }
-                             else {
-                                 Toast.makeText(MainActivity.this, "Sign In Failed!", Toast.LENGTH_LONG).show();
-                             }
-                         }
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(MainActivity.this, "Sign In Failed!", Toast.LENGTH_LONG).show();
+                            }
+                        }
                     });
         }
 
     }
 
-    public void signUpButton(View view){
+    public void signUpButton(View view) {
         // uncomment before submission
-        Intent intent = new Intent(this, SignUpActivity.class);
-        startActivity(intent);
+        //Intent intent = new Intent(this, SignUpActivity.class);
+        //startActivity(intent);
 
         // short cut to home for testing purposes
-        //Intent intent = new Intent(this, HomeActivity.class);
-        //startActivity(intent);
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 
 
